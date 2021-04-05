8729205e0bffee5d32d4d3a8c96001e353cd9274
dsunderg <vijayadinesh93@gmail.com>
2021-04-05 22:46:34 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Udacity_intermediate_python_meme_generator
-Udacity Meme generator project for Intermediate Python course
+###Running the Flask APP
+1. create a virtual environment `virtualvenv venv`
+2. activate the virtualenv `source ./venv/bin/activate`
+3. run `pip install -r requirements.txt`
+4. post successful complettion of this run the app, `python app.py`
+
+access the application http://127.0.0.1:1947 
+
+Python version need 3.6b and above
+
---
+++ app.py
@@ -0,0 +1,94 @@
+import os
+from random import choice
+from shutil import rmtree
+
+import requests
+from flask import Flask, render_template, request
+
+from meme import MemeEngine
+from ingestors import Ingestor
+
+host_config ={
+    "host": "127.0.0.1",
+    "port":1947,
+    "debug":False, #set it to true in dev stage for debugging
+    "threaded":True,    
+}
+
+app = Flask(__name__)
+
+
+static_dir = "./static"
+if os.path.exists(static_dir):
+    rmtree(static_dir)
+meme = MemeEngine(static_dir)
+
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
+
+def setup():
+    """ Load all resources """
+    quote_files = ["./_data/DogQuotes/DogQuotesTXT.txt",
+                   "./_data/DogQuotes/DogQuotesDOCX.docx",
+                   "./_data/DogQuotes/DogQuotesPDF.pdf",
+                   "./_data/DogQuotes/DogQuotesCSV.csv"]
+    all_quotes = []
+    for f in quote_files:
+        try:
+            all_quotes.extend(Ingestor.parse(f))
+        except ValueError as error:
+            print(f"ValueError: {error}")
+
+    images_path = "./_data/Photos/Dog/"
+    all_images = []
+    for root, dirs, files in os.walk(images_path):
+        all_images = [os.path.join(root, name) for name in files]
+    return all_quotes, all_images
+
+
+quotes, images = setup()
+
+
+@app.route("/health")
+def health_check():
+    """Simple application health check API"""
+    return "Healthy", 200
+
+@app.route("/")
+def meme_rand():
+    """ Generate a random meme """
+    img = choice(images)
+    quote = choice(quotes)
+    path = meme.make_meme(img, quote.body, quote.author)
+    return render_template("meme.html", path=path)
+
+
+@app.route("/create", methods=["GET"])
+def meme_form():
+    """ User input for meme information """
+    return render_template("meme_form.html")
+
+
+@app.route("/create", methods=["POST"])
+def meme_post():
+    """ Create a user defined meme """
+    img = "./temp_image.jpg"
+    image_url = request.form.get("image_url")
+    img_data = requests.get(image_url, stream=True).content
+    with open(img, "wb") as f:
+        f.write(img_data)
+
+    body = request.form.get("body", "")
+    author = request.form.get("author", "")
+    path = meme.make_meme(img, body, author)
+    print(path)
+    os.remove(img)
+    return render_template("meme.html", path=path)
+
+
+if __name__ == "__main__":
+    app.run(**host_config)
